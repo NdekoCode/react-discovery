5e4f648d14aa5af8586b3fa0c96fd16eac24fcb6
NdekoCode <arickbulakali@gmail.com>
2025-08-05 00:19:18 +0200

    refactor: add shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+// https://vite.dev/config/
+import path from "node:path";
 import { defineConfig } from "vite";
 
 import tailwindcsss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcsss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
