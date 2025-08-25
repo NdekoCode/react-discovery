58890c6a61d14dda2077041e54ec9b7c26ea5607
NdekoCode <arickbulakali@gmail.com>
2025-08-25 23:29:39 +0200

    feat: implement blog page with post fetching and loading state, and add post image asset

--- src/pages/blog/page.tsx
+++ src/pages/blog/page.tsx
@@ -1,70 +1,107 @@
 import { useDocumentTitle } from "@/hooks/use-document-title";
+import { useFetch } from "@/hooks/use-fetch";
+import { Post } from "@/lib/types/generics/posts";
 
 export default function BlogPage() {
   useDocumentTitle("Blog page");
+  const { data, loading } = useFetch<{ posts: Post[] }>(
+    "https://dummyjson.com/posts?limit=12"
+  );
   return (
-    <section className="py-16 md:py-32">
-      <div className="mx-auto max-w-5xl space-y-8 px-6 md:space-y-16">
-        <h2 className="relative z-10 max-w-xl text-4xl font-medium lg:text-5xl">
-          Blog
-        </h2>
-        <div className="grid gap-6 sm:grid-cols-2 md:gap-12 lg:gap-24">
-          <div className="relative mb-6 sm:mb-0">
-            <div className="bg-linear-to-b aspect-76/59 relative rounded-2xl from-zinc-300 to-transparent p-px dark:from-zinc-700">
-              <img
-                src="https://images.unsplash.com/photo-1755331039789-7e5680e26e8f?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                className="hidden rounded-[15px] dark:block"
-                alt="payments illustration dark"
-                width={1207}
-                height={929}
-              />
-              <img
-                src="https://images.unsplash.com/photo-1755398103904-1c1f4466b535?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                className="rounded-[15px] shadow dark:hidden"
-                alt="payments illustration light"
-                width={1207}
-                height={929}
-              />
-            </div>
-          </div>
-
-          <div className="relative space-y-4">
-            <p className="text-muted-foreground">
-              Gemini is evolving to be more than just the models.{" "}
-              <span className="text-accent-foreground font-bold">
-                It supports an entire ecosystem
-              </span>{" "}
-              — from products innovate.
-            </p>
-            <p className="text-muted-foreground">
-              It supports an entire ecosystem — from products to the APIs and
-              platforms helping developers and businesses innovate
-            </p>
-
-            <div className="pt-6">
-              <blockquote className="border-l-4 pl-4">
-                <p>
-                  Using TailsUI has been like unlocking a secret design
-                  superpower. It's the perfect fusion of simplicity and
-                  versatility, enabling us to create UIs that are as stunning as
-                  they are user-friendly.
-                </p>
-
-                <div className="mt-6 space-y-3">
-                  <cite className="block font-medium">John Doe, CEO</cite>
-                  <img
-                    className="h-5 w-fit dark:invert"
-                    src="https://html.tailus.io/blocks/customers/nvidia.svg"
-                    alt="Nvidia Logo"
-                    height="20"
-                    width="auto"
-                  />
-                </div>
-              </blockquote>
-            </div>
+    <>
+      {loading ? (
+        <div className="fixed inset-0 bg-black/10 flex items-center justify-center">
+          <div className="flex justify-center items-center size-11 text-sm font-medium rounded-lg border border-transparent bg-transparent text-white focus:outline-hidden disabled:opacity-50 disabled:pointer-events-none">
+            <span
+              className="animate-spin inline-block size-4 border-3 border-blue-700 border-t-transparent text-white rounded-full"
+              role="status"
+              aria-label="loading"
+            >
+              <span className="sr-only">Loading...</span>
+            </span>
           </div>
         </div>
-      </div>
-    </section>
+      ) : (
+        <section className="py-16 md:py-32 flex flex-col items-center justify-center">
+          {data ? (
+            <div className="mx-auto max-w-5xl space-y-8 px-6 md:space-y-16">
+              <h2 className="relative z-10 max-w-xl text-4xl font-medium lg:text-5xl">
+                Blog
+              </h2>
+              {/* Card Blog */}
+              <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+                {/* Title */}
+                <div className="max-w-2xl text-center mx-auto mb-10 lg:mb-14">
+                  <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">
+                    Read our latest news
+                  </h2>
+                  <p className="mt-1 text-gray-600 dark:text-neutral-400">
+                    We've helped some great companies brand, design and get to
+                    market.
+                  </p>
+                </div>
+                {/* End Title */}
+
+                {/* Grid */}
+                <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-10 lg:mb-14">
+                  {/* Card */}
+                  {data.posts.map((post) => (
+                    <a
+                      className="group flex flex-col bg-white border border-gray-200 shadow-2xs rounded-xl hover:shadow-md focus:outline-hidden focus:shadow-md transition dark:bg-neutral-900 dark:border-neutral-800"
+                      href={"#post-" + post.id}
+                      key={post.id}
+                    >
+                      <div className="aspect-w-16 aspect-h-9">
+                        <img
+                          className="w-full object-cover rounded-t-xl"
+                          src="/post-image.jpg"
+                          alt={post.title}
+                        />
+                      </div>
+                      <div className="p-4 md:p-5">
+                        <p className="mt-2 text-xs uppercase text-gray-600 dark:text-neutral-400">
+                          {post.title}
+                        </p>
+                        <h3 className="mt-2 text-lg font-medium text-gray-800 group-hover:text-blue-600 dark:text-neutral-300 dark:group-hover:text-white">
+                          {post.body.substring(0, 75)}...
+                        </h3>
+                      </div>
+                    </a>
+                  ))}
+                  {/* End Card */}
+                </div>
+                {/* End Grid */}
+              </div>
+              {/* End Card Blog */}
+            </div>
+          ) : (
+            <div className="min-h-60 flex flex-col">
+              <div className="flex flex-auto flex-col justify-center items-center p-4 md:p-5">
+                <svg
+                  className="size-10 text-gray-500 dark:text-neutral-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <line x1="22" x2="2" y1="12" y2="12"></line>
+                  <path d="M5.45 5.11 2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"></path>
+                  <line x1="6" x2="6.01" y1="16" y2="16"></line>
+                  <line x1="10" x2="10.01" y1="16" y2="16"></line>
+                </svg>
+                <p className="mt-2 text-sm text-gray-800 dark:text-neutral-300">
+                  No data to show
+                </p>
+              </div>
+            </div>
+          )}
+        </section>
+      )}
+    </>
   );
 }
